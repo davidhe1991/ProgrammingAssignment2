bf017f5242bb89a0d676e62393593abf3bd56893
David Hegit config --global user.name David <sh562@cornell.edu>
2014-04-27 20:12:13 -0700

    editting cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## The two functions below are used to create a special object that stores a numeric vector and caches its mean
 
 ## Write a short comment describing this function
+## makeCacheMatrix function will create a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -9,6 +11,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## cacheSolve function computes the inverse of the special matrix returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve function retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
